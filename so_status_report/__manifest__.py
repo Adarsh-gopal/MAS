449050b5253316a85a6799e83d29bd24414a3b31
adarsh <adarsh@prixgen.com>
2021-05-31 21:53:54 +0530

    so_status_report

---
+++ so_status_report/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'SO Status Report',
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',    
+    
+    'version': '14.0.1',
+    
+    'category': 'Sales',
+
+    'summary': 'This module is useful to show the sale order lines with status of Delivery and Invoice',
+
+    'description': """This module is useful to show the sale order lines with status of Delivery and Invoice""",
+    
+    'depends': ['sale'],
+    
+    'data': [
+        'views/sale_order_line.xml',
+    ],
+    
+    'auto_install': False,
+    'installable' : True,
+    'application': True,
+}
