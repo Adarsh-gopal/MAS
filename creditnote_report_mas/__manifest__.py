3f912f735c0dfbdbb99e6bb145af0f81d1265491
adarsh <adarsh@prixgen.com>
2021-06-07 21:47:08 +0530

    creditnote_report_mas

---
+++ creditnote_report_mas/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'MAS credit note report Template',
+    'version': '14.0.0.4',
+    'description': """This module consists, the credit note Templates""",
+    'category': 'Localization',
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',
+    'depends': ['account','l10n_in','web','base','product','sale',],
+    'data': [
+        'reports/creditnote_report.xml',
+        'views/header_footer.xml',   
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
