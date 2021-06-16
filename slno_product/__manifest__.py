ed6cb384852bf8fec5ffa47a296deec10abe63d3
adarsh <adarsh@prixgen.com>
2021-06-16 18:41:46 +0530

    slno_product

---
+++ slno_product/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'SLNO For Add a Product',
+    'version': '14.0.1',
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',
+
+    'category': 'Sales',
+    'summary': 'SLNO For Add a Product',
+    'description': """SLNO For Add a Product""",
+    'depends': ['base','sale'],
+    'data': [
+        'views/model_slno_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ slno_product/models/model_slno.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    serial_no = fields.Integer(string='Sl No',compute="_compute_sl")
+
+    @api.depends('order_id.order_line')
+    def _compute_sl(self):
+        var = 1
+        for l in self:
+            if l.serial_no == 0 and l.display_type not in ('line_section','line_note'):
+                l.serial_no = var + l.serial_no
+                var += 1
+            else:
+                l.serial_no = 0
+
