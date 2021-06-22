98f39d1992e387bb3e507a3ea9a7ad00503770f1
adarsh <adarsh@prixgen.com>
2021-06-22 18:07:40 +0530

    custom_mrp_validation

---
+++ custom_mrp_validation/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Work order validation",
+
+    'summary': """
+        Validation for work order""",
+
+    'description': """
+        Validation for work order
+    """,
+
+    'author': "Prixgen Tech Solutions Pvt. Ltd.",
+    'company': "Prixgen Tech Solutions Pvt. Ltd.",
+    'website': "https://www.prixgen.com",
+
+    'category': 'Manufacturing',
+    'version': '14.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mrp'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+    ],
+    
+}
