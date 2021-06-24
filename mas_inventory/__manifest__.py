6c0cd06a81539f02b7901cb24ea4b3224731ceff
adarsh <adarsh@prixgen.com>
2021-06-24 18:15:16 +0530

    mas_inventory

---
+++ mas_inventory/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Internal Note Filter For MAS",
+
+    'summary': """
+       Internal Note Filter For MAS""",
+
+    'description': """Internal Note Filter For MAS""",
+
+    'author': "Prixgen Tech Solutions Pvt. Ltd.",
+    'company': "Prixgen Tech Solutions Pvt. Ltd.",
+    'website': "https://www.prixgen.com",
+
+
+    'category': 'Customization',
+    'version': '14.0.0.1',
+
+    'depends': ['base','product'],
+
+    'data': [
+        'views/search_view.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
