a7e87a887d839c467c53c1d38963e81b6dd67ec2
adarsh <adarsh@prixgen.com>
2021-06-29 10:23:35 +0530

    serail_no_base

---
+++ serail_no_base/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Serial_Number For Add Product',
+    'version': '14.0.0.1',
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',
+
+    'category': 'Base',
+    'summary': 'Serial No  For Add a Product',
+    'description': """Serial No For Add a Product""",
+    'depends': ['base','sale','purchase'],
+    'data': [
+        'views/model_slno_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
