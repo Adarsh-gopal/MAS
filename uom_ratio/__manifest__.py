b9fc69f421d7385de00ee6188c6a9fcc4ba85b6f
adarsh <adarsh@prixgen.com>
2021-06-30 16:53:29 +0530

    uom_ratio

---
+++ uom_ratio/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Automated UOM Ratio calculation',
+    'category': 'Tools',
+    'summary': 'This module is used to calculate the UOM Ratio based on Length, Width & Thickness formula."',
+    'version': '14.0.0.2',
+    'description': """This module is used to calculate the UOM Ratio based on Length, Width & Thickness formula.""",
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',
+    'depends': ['uom'],
+    'data': ['views/uom.xml'],
+    'installable': True,
+    'auto_install': True,
+  
+}
