4b5c32c7c459ae84e62b17bea299063c473396e5
Prixgen <36538394+Adarsh-gopal@users.noreply.github.com>
2021-07-08 18:24:49 +0530

    Sale_register_4

--- sale_register_new/models/sale_register_new.py
+++ sale_register_new/models/sale_register_new.py
@@ -48,8 +48,8 @@ class AccountMoveLine(models.Model):
                                 line.cgst_rate  = each_tcs.amount if each_tcs.amount else 0
                                 line.cgst_amount = (line.price_subtotal *line.cgst_rate)/100
                             if each_tcs.tax_group_id.name == 'TCS':
-                                line.tcs_rate  = each_tcs.amount if each_tcs.amount else 0.0
-                                line.tcs_amount  = (line.price_subtotal *line.tcs_rate)/100
+                                line.tds_rate  = each_tcs.amount if each_tcs.amount else 0.0
+                                line.tds_amount  = (line.price_subtotal *line.tcs_rate)/100
                         # if each_line.tax_group_id.name == 'RCM':
                             
                         #     for rcm_per in each_line.children_tax_ids:
