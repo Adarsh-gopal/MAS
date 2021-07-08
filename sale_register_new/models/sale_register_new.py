be6689f5a1753af29529ed878e03b14e7b626ddf
Prixgen <36538394+Adarsh-gopal@users.noreply.github.com>
2021-07-08 18:12:28 +0530

    Sale_register_1

--- sale_register_new/models/sale_register_new.py
+++ sale_register_new/models/sale_register_new.py
@@ -40,8 +40,8 @@ class AccountMoveLine(models.Model):
                                 line.igst_rate  = each_tcs.amount if each_tcs.amount else 0.0
                                 line.igst_amount  = (line.price_subtotal *line.igst_rate)/100
                             if each_tcs.tax_group_id.name == 'TCS':
-                                line.tds_rate  = each_tcs.amount if each_tcs.amount else 0.0
-                                line.tds_amount = (line.price_subtotal *line.tds_rate)/100
+                                line.tcs_rate  = each_tcs.amount if each_tcs.amount else 0.0
+                                line.tcs_amount = (line.price_subtotal *line.tds_rate)/100
                             if each_tcs.tax_group_id.name == 'SGST' or each_tcs.tax_group_id.name == 'CGST':
                                 line.sgst_rate  = each_tcs.amount if each_tcs.amount else 0
                                 line.sgst_amount = (line.price_subtotal *line.sgst_rate)/100
@@ -64,9 +64,9 @@ class AccountMoveLine(models.Model):
                             if each_tcs.tax_group_id.name == 'IGST':
                                 line.igst_rate  = each_tcs.amount if each_tcs.amount else 0.0
                                 line.igst_amount  = (line.price_subtotal *line.igst_rate)/100
-                            if each_tcs.tax_group_id.name == 'TDS':
-                                line.tds_rate  = each_tcs.amount if each_tcs.amount else 0.0
-                                line.tds_amount  = (line.price_subtotal *line.tds_rate)/100
+                            if each_tcs.tax_group_id.name == 'TCS':
+                                line.tcs_rate  = each_tcs.amount if each_tcs.amount else 0.0
+                                line.tcs_amount  = (line.price_subtotal *line.tds_rate)/100
                             if each_tcs.tax_group_id.name == 'SGST' or each_tcs.tax_group_id.name == 'CGST':
                                 line.sgst_rate  = each_tcs.amount if each_tcs.amount else 0.0
                                 line.sgst_amount = (line.price_subtotal *line.sgst_rate)/100
