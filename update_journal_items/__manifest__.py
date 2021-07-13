729edcfb7fe5b40c0b4adf69eb618278cb25cc2f
Prixgen <36538394+Adarsh-gopal@users.noreply.github.com>
2021-07-13 14:44:06 +0530

    update_journal_items

---
+++ update_journal_items/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Update Analytic accounts for journal items",
+
+    'summary': """ Update Analytic accounts for the journal items which 
+     no analytic accounts in the invoice lines
+        
+        """,
+
+    'description': """
+        Update Analytic accounts for the journal items which has no 
+        analytic accounts in the invoice lines
+    """,
+
+    'author': 'Prixgen Tech Solutions Pvt. Ltd.',
+    'website': 'https://www.prixgen.com',
+    'company': 'Prixgen Tech Solutions Pvt. Ltd.',
+
+    
+    'category': 'Uncategorized',
+    'version': '14.0.0.1',
+
+   
+    'depends': ['base','account'],
+
+  
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_move.xml',
+       
+    ],
+}
