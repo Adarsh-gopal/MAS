8628dc9a7fdaa6a7ad452be1b3f7af9b54e03460
Prixgen <36538394+Adarsh-gopal@users.noreply.github.com>
2021-07-13 14:34:30 +0530

    update_journal_items

---
+++ update_journal_items/models/account_move.py
@@ -0,0 +1,16 @@
+from odoo import api,models,fields
+
+
+class UpdateAnalyticAccount(models.TransientModel):
+	_name='update.journal'
+	_description='update.journal'
+
+	def update_journals(self):
+		line = self._context.get('active_ids')
+
+		self.env.cr.execute("""UPDATE account_move_line 
+				set debit = 0.0, credit = 0.0,amount_currency =0.0  WHERE id IN %s""",(tuple(line),))
+
+
+
+            
