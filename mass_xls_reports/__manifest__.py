d147ea631034a899a3aced0a034bab1e692e7cfd
Prixgen <36538394+Adarsh-gopal@users.noreply.github.com>
2021-07-15 15:37:47 +0530

    mass_xls_reports

--- mass_xls_reports/__manifest__.py
+++ mass_xls_reports/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Mas XLSX Reports',
     'version': '14.0.1.5',
     'category': 'Customization',
-    'description': """
+    'description': u"""
     This module generates xlsx reports for sale orders	
     """,
     'author': 'Prixgen Tech Solutions Pvt. Ltd.',
